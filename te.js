5003c75853a92ea400ebb46e83569b64c16ab2f9
Maxim Shelupenko <maxim.shelupenko@gmail.com>
2013-04-09 22:25:21 +0300

    The functionality to activate templates passing the element selector, parent selector and array of attributes is implemented.

--- te.js
+++ te.js
@@ -8,7 +8,25 @@
 * http://en.wikipedia.org/wiki/MIT_License
 */
 window.te = (function () {
-  var te = function() {
+  var te = function (elSel, parentSel, attrs) {
+
+    elSel = typeof elSel !== 'undefined' ? elSel : "template";
+    parentSel = typeof parentSel !== 'undefined' ? parentSel : "body";
+    attrs = typeof attrs !== 'undefined' ? attrs : [];
+
+    var templ = document.querySelector(elSel),
+        parent = document.querySelector(parentSel);
+
+    if (templ !== null && parent !== null && supportsTemplate(templ)) {
+      for(var i = 0; i < attrs.length; i++) {
+        var childEl = templ.content.querySelector(attrs[i].s);
+        if (childEl !== null) {
+          childEl[attrs[i].a] = attrs[i].v;
+        }
+      }
+    var newNode = templ.content.cloneNode(true);
+      parent.appendChild(newNode);
+    }
   };
 
   var supportsTemplate =  function(el) {
