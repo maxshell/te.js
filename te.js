b8f1d79eef8704ada0ea8261b840b779981127cb
Maxim Shelupenko <maxim.shelupenko@gmail.com>
2013-04-10 14:47:32 +0300

    te.js is passed through the jslint and jshint

--- te.js
+++ te.js
@@ -7,47 +7,44 @@
 * Released under the MIT license.
 * http://en.wikipedia.org/wiki/MIT_License
 */
-window.te = (function () {
-  var templSel = 'template',
-      undefType = 'undefined';
+var te = (function (document) {
+    'use strict';
+    var templSel = 'template',
+        supportsTemplate = function (el) {
+            el = el !== undefined ? el : document.createElement(templSel);
+            return el.hasOwnProperty('content');
+        },
+        init = function (elSel, attrs) {
+            elSel = elSel !== undefined ? elSel : templSel;
+            attrs = attrs !== undefined ? attrs : [];
+            var templ = document.querySelector(elSel),
+                i,
+                childEl,
+                result = null;
+            if (templ !== null && supportsTemplate(templ)) {
+                for (i = 0; i < attrs.length; i = i + 1) {
+                    childEl = templ.content.querySelector(attrs[i].s);
+                    if (childEl !== null) {
+                        childEl[attrs[i].a] = attrs[i].v;
+                    }
+                }
+                result = templ;
+            }
+            return result;
+        },
+        te = function (parentSel, elSel, attrs) {
+            if (parentSel !== undefined) {
+                var parent = document.querySelector(parentSel),
+                    templ = init(elSel, attrs),
+                    newNode;
+                if (parent !== null && templ !== null) {
+                    newNode = templ.content.cloneNode(true);
+                    parent.appendChild(newNode);
+                }
+            }
+        };
+    te.init = init;
+    te.supportsTemplate = supportsTemplate;
 
-  var te = function (parentSel, elSel, attrs) {
-    if (typeof parentSel !== undefType) {
-      var parent = document.querySelector(parentSel),
-          templ = init(elSel, attrs);
-      if (parent !== null && templ !== null) {
-        var newNode = templ.content.cloneNode(true);
-        parent.appendChild(newNode);
-      }
-    }
-  };
-
-  var init = function (elSel, attrs) {
-    elSel = typeof elSel !== undefType ? elSel : templSel;
-    attrs = typeof attrs !== undefType ? attrs : [];
-
-    var templ = document.querySelector(elSel),
-        result = null;
-
-    if (templ !== null && supportsTemplate(templ)) {
-      for (var i = 0; i < attrs.length; i++) {
-        var childEl = templ.content.querySelector(attrs[i].s);
-        if (childEl !== null) {
-          childEl[attrs[i].a] = attrs[i].v;
-        }
-      }
-      result = templ;
-    }
-    return result;
-  };
-
-  var supportsTemplate = function (el) {
-    el = typeof el !== undefType ? el : document.createElement(templSel);
-    return 'content' in el;
-  };
-  
-  te.init = init;
-  te.supportsTemplate = supportsTemplate;
-
-  return te;
-}());
+    return te;
+}(document));
